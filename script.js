f57e5c41735af76ee169337b8936d588a4bae6e2
chardayneal <106034224+chardayneal@users.noreply.github.com>
2023-03-03 08:47:05 -0800

    Initial Commit and Create UI
    Removed logic to play 5 rounds
    Created buttons with event listeners to play one round
    Updates and displays score after each round

--- script.js
+++ script.js
@@ -1,43 +1,46 @@
+let cpuPoints = document.querySelector('#cpu-pnts');
+let myPoints = document.querySelector('#player-pnts');
+
 //score of player&cpu A N D round tracker
-let round = 1;
 let player_points = 0;
 let cpu_points = 0;
 
 // game of rock, paper, scissors
 
 function play(player, cpu) {
-    console.log(`Round ${round} `)
-    if (player != 'rock' || 'paper' || 'scissors') {
-        alert("Invalid Selection");
-    } else {
-        console.log("You Selected: " + player);
-    }
+    console.log("You Selected: " + player);
     console.log("Computer's Choice: " + cpu)
     switch (player) {
         case 'rock':
             if (cpu == 'paper') {
-                return cpu_points++;
+                cpu_points++;
+                return cpuPoints.textContent = `Computer: ${cpu_points}`;
             } 
             else if (cpu == 'scissors') {
-                return player_points++;
+                player_points++;
+                return myPoints.textContent = `My Score: ${player_points}`;
             } else {
-                return "Tie!";
+                return console.log("Tie!");
             }
 
         case 'paper':
             if (cpu == 'rock') {
-                return player_points++;
+                player_points++;
+                return myPoints.textContent = `My Score: ${player_points}`;
             } else if (cpu == 'scissors') {
-                return cpu_points++;
+                cpu_points++;
+                return cpuPoints.textContent = `Computer: ${cpu_points}`;
             } else {
                 return "Tie!";
             }
             
         case 'scissors': 
             if (cpu == 'rock') {
-                return cpu_points++;
+                cpu_points++;
+                return cpuPoints.textContent = `Computer: ${cpu_points}`;
             } else if (cpu == 'paper') {
-                return player_points++;
+                player_points++;
+                return myPoints.textContent = `My Score: ${player_points}`;
             } else {
                 return "Tie!";
             }
@@ -51,19 +54,11 @@ function getComputerChoice() {
     return selection[i];
 }
 
-// plays five rounds and prints winner of all rounds
-function game() {
-    for (let i = 0; i < 5; i++) {
-        play((prompt(`Round ${round}: Which would you like to choose? (rock, paper, scissors) `)).toLowerCase(), getComputerChoice());
-        round++;
-    }
-    if (player_points > cpu_points) {
-        console.log("You Win!");
-    } else if (player_points < cpu_points){
-        console.log("Computer Wins!")
-    } else {
-        console.log("It's a Tie!")
-    }
-}
+// eventListener for buttons
+const buttons = document.querySelectorAll('button');
+buttons.forEach((button) => {
+    button.addEventListener('click', () => {
+        play(button.id, getComputerChoice());
+    });
+});
 
-game();
