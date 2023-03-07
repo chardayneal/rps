8ba350441b357e189fbbfa7e8e87b3335fb19741
chardayneal <106034224+chardayneal@users.noreply.github.com>
2023-03-07 14:50:51 -0800

    Update script.js
    Created getResults(), getAnswer(), playerWins(), cpuWins()
    all for efficiency
    
    Game ends after player reaches 5 points; however doesn't display
    the winning round results.

--- script.js
+++ script.js
@@ -13,7 +13,7 @@ let selection = ['rock', 'paper', 'scissors'];
 let cpuPoints = document.querySelector('#cpu-pnts');
 let myPoints = document.querySelector('#player-pnts');
 
-//score of player&cpu A N D round tracker
+//score of player&cpu tracker
 let player_points = 0;
 let cpu_points = 0;
 
@@ -24,6 +24,106 @@ const img = document.createElement('img');
 img.setAttribute('class', 'selected');
 const img2 = document.createElement('img');
 img2.setAttribute('class', 'selected');
+
+// eventListener for buttons
+const buttons = document.querySelectorAll('button');
+buttons.forEach((button) => {
+    button.addEventListener('click', () => {
+        play(button.id, getComputerChoice());
+    });
+}); 
+
+// select computer's choice
+function getComputerChoice() { 
+    let i = Math.floor(Math.random() * selection.length);
+    return selection[i];
+}
+
+function play(player, cpu) {
+
+// S T E P 1 -> remove the header
+    if(body.contains(header)) {
+        body.removeChild(header);
+    } 
+
+// S T E P 2 -> assigns correct image to player selection
+    getImage(player, cpu);
+
+// S T E P 3 -> rps logic
+    switch (player) {
+        case 'rock':
+            if (cpu == 'paper') {
+                cpuWins(player, cpu);
+            } else if (cpu == 'scissors') {
+                playerWins(player, cpu);
+            } else {
+                winner_text = "It's a tie!";
+                getResults();
+                if (div.contains(answer)) {
+                    div.removeChild(answer);
+                }
+            }
+            if (cpu_points == 5 || player_points == 5) {
+                if (cpu_points == 5) {
+                    alert('Sorry, you lose.');
+                    resetGame();
+                } else {
+                    alert("Yay, you won!");
+                    resetGame();
+                }
+            }
+            break;
+
+        case 'paper':
+            if (cpu == 'rock') {
+                playerWins(player, cpu);
+            } else if (cpu == 'scissors') {
+                cpuWins(player, cpu);
+            } else {
+                winner_text = "It's a tie!";
+                if (div.contains(answer)) {
+                    div.removeChild(answer);
+                }
+            }
+            if (cpu_points == 5 || player_points == 5) {
+                if (cpu_points == 5) {
+                    alert('Sorry, you lose.');
+                    resetGame();
+                } else {
+                    alert("Yay, you won!");
+                    resetGame();
+                }
+            }
+            break;
+            
+        case 'scissors': 
+            if (cpu == 'rock') {
+                cpuWins(player, cpu);
+                getResults();
+            } else if (cpu == 'paper') {
+                playerWins(player, cpu);
+                getResults();
+            } else {
+                winner_text = "It's a tie!";
+                getResults();
+                if (div.contains(answer)) {
+                    div.removeChild(answer);
+                }
+            }
+            if (cpu_points == 5 || player_points == 5) {
+                if (cpu_points == 5) {
+                    alert('Sorry, you lose.');
+                    resetGame();
+                } else {
+                    alert("Yay, you won!");
+                    resetGame();
+                }
+            }
+            break;
+            }       
+    }
+    
+
 function getImage(choice1, choice2) {
     images.forEach((image) => {
         if (image.id == `${choice1}_img` && image.id == `${choice2}_img`) {
@@ -36,97 +136,51 @@ function getImage(choice1, choice2) {
         }
     });
 }
+
+//updates cpu's score, winning text, UI score, answer img
+function cpuWins(player, cpu) {
+    cpu_points++;
+    winner_text = `${cpu}  beats  ${player}`;
+    getAnswer('wrong');
+    cpuPoints.textContent = `Computer: ${cpu_points}`;
+    return getResults();
+}
+
 const answer = document.createElement('img');
 answer.getAttribute('id', 'answer');
 function getAnswer(source) {
-    answer.src = `./images/${source}.png`;
+    return answer.src = `./images/${source}.png`;
 }
-// game of rock, paper, scissors
 
-function play(player, cpu) {
-    if(body.contains(header)) {
-        body.removeChild(header);
-        body.removeChild(para);
-    } else if (body.contains(para)) {
-        body.removeChild(para);
+// displays results to UI
+function getResults() {
+    winner_div.textContent = winner_text;
+    div.appendChild(winner_div);
+    div.appendChild(answer);
+    player_img.appendChild(img);
+    cpu_img.appendChild(img2);
+}
+
+// updates player's score, winning text, UI score, answer img
+function playerWins(player, cpu) {
+    player_points++;
+    winner_text = `${player}  beats  ${cpu}`;
+    getAnswer('check');
+    myPoints.textContent = `My Score: ${player_points}`;
+    return getResults();
+}
+
+
+// resets Game 
+function resetGame() {
+    player_points = 0;
+    cpu_points = 0;
+    myPoints.textContent = `My Score: ${player_points}`;
+    cpuPoints.textContent = `Computer: ${cpu_points}`;
+    if (div.contains(answer)) {
+        div.removeChild(answer);
     }
-
-    getImage(player, cpu);
-
-    switch (player) {
-        case 'rock':
-            if (cpu == 'paper') {
-                cpu_points++;
-                winner_text = `${cpu}  beats  ${player}`;
-                getAnswer('wrong');
-                return cpuPoints.textContent = `Computer: ${cpu_points}`;
-            } 
-            else if (cpu == 'scissors') {
-                player_points++;
-                winner_text = `${player}  beats  ${cpu}`;
-                getAnswer('check');
-                return myPoints.textContent = `My Score: ${player_points}`;
-            } else {
-                winner_text = "It's a tie!";
-                return console.log("Tie!");
-            }
-
-        case 'paper':
-            if (cpu == 'rock') {
-                player_points++;
-                winner_text = `${player}  beats  ${cpu}`;
-                getAnswer('check');
-                return myPoints.textContent = `My Score: ${player_points}`;
-            } else if (cpu == 'scissors') {
-                cpu_points++;
-                winner_text = `${cpu}  beats  ${player}`;
-                getAnswer('wrong');
-                return cpuPoints.textContent = `Computer: ${cpu_points}`;
-            } else {
-                winner_text = "It's a tie!";
-                return "Tie!";
-            }
-            
-        case 'scissors': 
-            if (cpu == 'rock') {
-                cpu_points++;
-                winner_text = `${cpu}  beats  ${player}`;
-                getAnswer('wrong');
-                return cpuPoints.textContent = `Computer: ${cpu_points}`;
-            } else if (cpu == 'paper') {
-                player_points++;
-                winner_text = `${player}  beats  ${cpu}`;
-                getAnswer('check');
-                return myPoints.textContent = `My Score: ${player_points}`;
-            } else {
-                winner_text = "It's a tie!";
-                return "Tie!";
-            }
-    }
-
+    player_img.removeChild(img);
+    cpu_img.removeChild(img2);
+    div.removeChild(winner_div);
 }
-
-// select computer's choice
-function getComputerChoice() { 
-    let i = Math.floor(Math.random() * selection.length);
-    return selection[i];
-}
-
-
-
-// eventListener for buttons
-const buttons = document.querySelectorAll('button');
-buttons.forEach((button) => {
-    button.addEventListener('click', () => {
-        play(button.id, getComputerChoice());
-        winner_div.textContent = winner_text;
-        div.appendChild(winner_div);
-        if (winner_text == "It's a tie!") {
-            div.removeChild(answer);
-        } else {
-            div.appendChild(answer);
-        }
-        player_img.appendChild(img);
-        cpu_img.appendChild(img2);
-    });
-}); 
